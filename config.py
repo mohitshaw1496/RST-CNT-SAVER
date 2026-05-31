69a18023c52539cfccfc051f5f7f2b0ed0189f1e
mohitshaw1496 <darkdictator691@gmail.com>
2026-06-01 00:48:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "35125044"))
+API_HASH = getenv("API_HASH", "443933fa1e307654bea9f06c1cf6727e")
+BOT_TOKEN = getenv("BOT_TOKEN", "8498039053:AAE6elLomCPsfoaw7QZnmmoQD47QSQZ-91U")
+OWNER_ID = int(getenv("OWNER_ID", "6502857759"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://mohitshaw1496_db_user:0dHZzKuRiiwEoL84@cluster0.jmg8pwu.mongodb.net/?appName=Cluster0")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1003751654557"))
+FORCESUB = getenv("FORCESUB", "https://t.me/Z_Vertex_01")
